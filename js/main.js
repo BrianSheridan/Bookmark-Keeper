26ffb87de7b3d7824ce9a5f247a11bfa7d560bd0
BrianSheridan <briansheridan732@gmail.com>
2017-12-12 21:22:23 +0000

    Javascript Testing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-alert(1);
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// save Bookmarker
+function saveBookmark(e){
+  //get form values
+  var siteName =document.getElementById('siteName').value;
+  console.log(siteName);
+  var siteUrl =document.getElementById('siteUrl').value;
+  console.log(siteName);
+
+  var bookmark = {
+      name: siteName,
+      url: siteUrl
+  }
+
+  // Local Storage Test
+  localStorage.setItem('test', 'Hello World');
+
+  // Prevent form from submitting
+  e.preventDefault();
+}
